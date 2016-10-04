424326808642e1c3a579abba4cf393b0b173996d
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-10-04 23:31:11 +0300

    removed stuff from init.el

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
-;;; https://harryrschwartz.com/2016/02/15/switching-to-a-literate-emacs-configuration.html
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;(package-initialize)
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
 
@@ -9,35 +13,9 @@
 
 (global-set-key (kbd "C--") 'ace-window)
 
-;;(require 'cl)
-
-
-;(require 'flymake)
-;    (setq flymake-max-parallel-syntax-checks 2)
-;    (setq flymake-number-of-errors-to-display 4)
-
 
 
 
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
-
-
-
-;;(load-theme 'tango-dark t)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (solarized-theme use-package flycheck magit swbuff ace-window company-php company org yaml-mode web-mode twig-mode tabbar sass-mode rainbow-mode phpcbf php+-mode neotree ggtags geben-helm-projectile flymake-yaml flymake-sass flymake-phpcs flymake-php flymake-css ac-php))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
