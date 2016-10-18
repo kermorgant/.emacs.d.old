d959e652a58f4775010076be0e34287f1776d9ef
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-10-18 15:14:19 +0300

    automatic by use-package

--- init.el
+++ init.el
@@ -19,3 +19,17 @@
 (setq ido-enable-flex-matching t)
 (setq ido-everywhere t)
 (ido-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (company-php nginx-mode multi-term org-bullets yaml-mode web-mode use-package-chords twig-mode swbuff solarized-theme scss-mode rainbow-mode projectile php-mode org neotree markdown-mode magit ggtags flymake-php flymake-css flycheck dockerfile-mode))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
