f4805921156add219de81aed671d6de732bd7dae
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-10-20 00:52:00 +0300

    mail config

--- imapnotify.gmail.js
+++ imapnotify.gmail.js
@@ -10,7 +10,9 @@ exports.port = 993;
 exports.tls = true;
 exports.tlsOptions = { "rejectUnauthorized": false };
 exports.username = "mikael.kermorgant@gmail.com";
-exports.password = getStdout("pass show email/gmail.com | head -n1");
+exports.password = getStdout("imap-pass -g mikael.kermorgant@gmail.com");
 exports.onNewMail = "/usr/bin/mbsync gmail";
-exports.onNewMailPost = "/usr/bin/notmuch new";
+exports.onNewMailPost = "emacsclient  -e '(mu4e-update-index)'";
 exports.boxes = [ "INBOX" ];
+// at some point, gpg should replace imap-pass
+// see http://code.lexarcana.com/posts/text-based-email-setup-with-mbsync-and-mu.html
