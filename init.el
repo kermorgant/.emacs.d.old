3c59d6d48a6a64e024be04fe8f54d646c24631ce
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-12-24 00:43:58 +0200

    load org mode fix

--- init.el
+++ init.el
@@ -9,8 +9,14 @@
   (add-to-list 'load-path "~/.emacs.d/lisp")
   (require 'use-package))
 
-;(require 'bind-key)                ;; if you use any :bind variant
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")  t)
+  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+  (add-to-list 'load-path "#/.emacs.d/lisp"))
+(package-initialize)
 
 
 (org-babel-load-file "~/.emacs.d/configuration.org")
