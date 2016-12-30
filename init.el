4ee9a03483555e370fb8884fe1b121be94791e6b
Mikael Kermorgant <mikael.kermorgant@gmail.com>
2016-12-30 20:16:10 +0200

    custom.el + smex

--- init.el
+++ init.el
@@ -2,24 +2,29 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;(package-initialize)
-
 
 (eval-when-compile
   (add-to-list 'load-path "~/.emacs.d/lisp")
   (require 'use-package))
 
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")  t)
-  (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-
-  (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
-  (add-to-list 'load-path "#/.emacs.d/lisp"))
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")  t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+(add-to-list 'load-path "#/.emacs.d/lisp")
 (package-initialize)
 
-(org-babel-load-file "~/.emacs.d/configuration.org")
+;; Make sure Org is installed
+(unless (package-installed-p 'org)
+  (package-refresh-contents)
+  (package-install 'org))
 
+;; Org plus contrib needs to be loaded before any org related functionality is called
+(unless (package-installed-p 'org-plus-contrib)
+  (package-refresh-contents)
+  (package-install 'org-plus-contrib))
+
+(org-babel-load-file "~/.emacs.d/configuration.org")
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -27,11 +32,10 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (ivy-hydra php-extras highlight-symbol php-eldoc elscreen-persist elscreen persp-projectile undo-tree find-file-in-project htmlize php-auto-yasnippets notmuch ox-html5slide ox-reveal auto-dictionary prodigy mu4e-maildirs-extension js2-mode scss-mode multi-term yaml-mode web-mode use-package twig-mode tabbar swbuff solarized-theme sass-mode rainbow-mode phpcbf php+-mode org-bullets org neotree magit ggtags geben-helm-projectile flymake-yaml flymake-sass flymake-phpcs flymake-php flymake-css flycheck dockerfile-mode company-php ace-window ac-php))))
+    (org-plus-contrib ivy-hydra php-extras highlight-symbol php-eldoc elscreen-persist elscreen persp-projectile undo-tree find-file-in-project htmlize php-auto-yasnippets notmuch ox-html5slide ox-reveal auto-dictionary prodigy mu4e-maildirs-extension js2-mode scss-mode multi-term yaml-mode web-mode use-package twig-mode tabbar swbuff solarized-theme sass-mode rainbow-mode phpcbf php+-mode org-bullets org neotree magit ggtags geben-helm-projectile flymake-yaml flymake-sass flymake-phpcs flymake-php flymake-css flycheck dockerfile-mode company-php ace-window ac-php))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
-(put 'upcase-region 'disabled nil)
